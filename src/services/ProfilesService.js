8acc083cfea9875e4aeae8069697a0260801f993
MeganHancock <mgnhnck@gmail.com>
2024-02-29 19:20:46 -0700

    actually getting profile by id

--- src/services/ProfilesService.js
+++ src/services/ProfilesService.js
@@ -1,7 +1,15 @@
+import { AppState } from "../AppState.js"
+import { Profile } from "../models/Profile.js"
+import { logger } from "../utils/Logger.js"
+import { api } from "./AxiosService.js"
+
 class ProfilesService {
 
     async getProfileById(profileId) {
-
+        const response = await api.get(`api/profiles/${profileId}`)
+        const newProfile = new Profile(response.data)
+        AppState.activeProfile = newProfile
+        logger.log(AppState.activeProfile)
     }
 }
 
